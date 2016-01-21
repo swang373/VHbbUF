41274101403b250149f79bfc78e0fcbd369adffc
Sean-Jiun Wang <churchguitarist@gmail.com>
2016-01-21 23:37:11 +0100

    Random search works, thanks Quentin

--- run2/BDT.py
+++ run2/BDT.py
@@ -23,25 +23,24 @@ DEFAULT_HYPERPARAMS = {
 class BDT(object):
 
     def __init__(self,
-                 name = 'BDT',
-                 job_name = 'job',
-                 factory = 'Silent:!DrawProgressBar:Transformations=I:AnalysisType=Classification',
+                 job_id = 'TMVAClassification',
                  dataset = '',  
                  hyperparams = {},
                  **kwargs):
 
         self.logger = logging.getLogger('BDT')
-        self.logger.info('Initialized for BDT "{!s}"'.format(name))
+        self.logger.info('Initialized for "{}"'.format(job_id))
         
-        self.name = name
-        self.job_name = job_name
-        self.factory = factory
+        # Set ROOT to batch mode.
+        ROOT.gROOT.SetBatch(1)
+
+        self.job_id = job_id
 
         if dataset is '':
             raise ValueError, 'No dataset provided.'
-        elif not os.path.isfile(DATASET_DIR + dataset + '.root'):
-            raise ValueError, 'Dataset file does not exist.'
         else:
+            if not os.path.isfile(DATASET_DIR + dataset + '.root'):
+                Dataset(dataset).make()
             self.dataset = dataset
 
         if not hyperparams:
@@ -52,11 +51,6 @@ class BDT(object):
 
     def train(self):
 
-        # Set ROOT to batch mode.
-        ROOT.gROOT.SetBatch(1)
-
-        ROOT.TMVA.Tools.Instance() 
-
         # Output Directory for Monitoring File
         try:
             os.makedirs(MONITOR_DIR)
@@ -65,17 +59,17 @@ class BDT(object):
                 raise
 
         # Change the TMVA weight file directory.
+        ROOT.TMVA.Tools.Instance()
         ioNames = ROOT.TMVA.gConfig().GetIONames()
         ioNames.fWeightFileDir = WEIGHT_DIR
-        # Uncomment below to change *.weight.xml to *.foo.xml.
+        # Lines below change *.weight.xml to *.foo.xml.
         #ioNames.fWeightFileExtension = 'foo'
 
-        # from datetime import datetime, include timestamp in file name.
         #timestamp = datetime.now().strftime('%d%b%Y_%H%M%S')
-        monitor_path = MONITOR_DIR + '{!s}_{!s}.root'.format(self.job_name, self.name)
+        monitor_path = MONITOR_DIR + self.job_id + '_BDT.root'
         monitor_file = ROOT.TFile(monitor_path, 'recreate')
 
-        factory = ROOT.TMVA.Factory(self.job_name, monitor_file, self.factory)
+        factory = ROOT.TMVA.Factory(self.job_id, monitor_file, 'Silent:!DrawProgressBar:Transformations=I:AnalysisType=Classification')
 
         # Register the classification variables.
         for var in VARIABLES.itervalues():
@@ -83,7 +77,6 @@ class BDT(object):
 
         # Load the dataset and add the training and validation samples.
         infile = ROOT.TFile(DATASET_DIR + self.dataset + '.root', 'read')
-
         for key in infile.GetListOfKeys():
             name = key.GetName()
             tree_class, tree_type = name.split('_')[-2:]
@@ -101,13 +94,15 @@ class BDT(object):
         factory.PrepareTrainingAndTestTree(sigCut, bkgCut, 'NormMode=None')
 
         bdt_options = ':'.join('{!s}={!s}'.format(*x) for x in self.hyperparams.iteritems())
-        factory.BookMethod(ROOT.TMVA.Types.kBDT, self.name, '!H:!V:' + bdt_options)
+        factory.BookMethod(ROOT.TMVA.Types.kBDT, 'BDT', '!H:!V:' + bdt_options)
 
         factory.TrainAllMethods()
         factory.TestAllMethods()
         factory.EvaluateAllMethods()
 
-        self.logger.info('BDT "{!s}" trained using options "{!s}"'.format(self.name, bdt_options))
+        monitor_file.Close()
+
+        self.logger.info('BDT "{!s}" trained using options "{!s}"'.format(self.job_id, bdt_options))
 
     def apply(self, model_name = ''):
     
@@ -129,13 +124,6 @@ if __name__ == '__main__':
     logging.basicConfig(level = logging.INFO,
                         format = '%(name)s(%(levelname)s) - %(message)s')
 
-    # Create the dataset if it does not exist.
-    dataset = '20Jan2016'
-    if not os.path.isfile(DATASET_DIR + dataset + '.root'):
-        Dataset(dataset).make()
-
-    # Run the BDT training and application (coming soon).
-    bdt = BDT(job_name = 'test', dataset = dataset)
-
+    bdt = BDT(dataset = '20Jan2016')
     bdt.train()
 
--- run2/randomsearch.py
+++ run2/randomsearch.py
@@ -1,5 +1,6 @@
 import logging
 import multiprocessing as mp
+import time
 
 from scipy import stats
 import numpy as np
@@ -29,9 +30,22 @@ def sample_hyperparams():
             hyperparams[name] = value
     return hyperparams
 
-def worker(options):
-    bdt = BDT(**options)
-    return bdt.train()
+class Worker(mp.Process):
+
+    """
+    github.com/qbuat/tauperf/blob/master/tauperf/parallel.py
+    Thank you, Quentin! Otherwise I'd be stuck in TMVA hell.
+    """
+
+    def __init__(self, func, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.result = mp.Queue()
+
+    def run(self):
+        self.func(*self.args, **self.kwargs)
 
 #------
 # Main
@@ -45,20 +59,42 @@ if __name__ == '__main__':
 
     logger = logging.getLogger('RandomSearch')
 
-    # Random Search Options
-    n_trials = 5
+    # Random Search Configuration
+    n_trials = 20
 
+    # BDT Configuration
     options = {
-        'job_name': 'test',
         'dataset': '20Jan2016',
     }
 
-    # Parallel Training
-    pool = mp.Pool(min(n_trials, mp.cpu_count()))
-    for i in xrange(n_trials): 
-        options['name'] = 'BDT_Trial{!s}'.format(i)
+    # Initialize Random Search Trials
+    bdts = []
+    for i in xrange(n_trials):
+        options['job_id'] = 'RandomSearch_Trial{!s}'.format(i)
         options['hyperparams'] = sample_hyperparams()
-        pool.apply_async(worker, (options,))
-    pool.close()
-    pool.join()
+        bdts.append(BDT(**options))
+    trials = [Worker(bdt.train) for bdt in bdts]
+
+    # Parallel Training
+    n_processes = min(n_trials, mp.cpu_count())
+    _processes = []
+    p = None
+
+    try:
+        while True:
+            n_active = len(mp.active_children())
+            while n_active < n_processes and len(trials) > 0:
+                p = trials.pop(0)
+                p.start()
+                _processes.append(p)
+                n_active = len(mp.active_children())
+            if len(trials) == 0 and n_active == 0:
+                break
+            time.sleep(0.1)
+    except KeyboardInterrupt, SystemExit:
+        if p is not None:
+            p.terminate()
+        for p in _processes:
+            p.terminate()
+        raise
 
